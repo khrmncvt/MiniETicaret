5c267689c808b86c04e73c08ee6dd619ea2467b8
Cevat <khrmncvt@gmail.com>
2025-07-10 22:57:12 +0300

    Client ve Api dosyaları oluşturuldu.

---
+++ ETicaretAPI/Infrastructure/ETicaretAPI.Persistence/Contexts/ETicaretApiDbContext.cs
@@ -0,0 +1,21 @@
+﻿using ETicaretAPI.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretApi.Persistence.Contexts
+{
+    public class ETicaretApiDbContext : DbContext
+    {
+        public ETicaretApiDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
---
+++ ETicaretAPI/Infrastructure/ETicaretAPI.Persistence/ServiceRegistration.cs
@@ -0,0 +1,19 @@
+﻿using ETicaretApi.Persistence.Contexts;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretApi.Persistence
+{
+    public static class ServiceRegistration
+    {
+        public static void AddPersistenceServices(this IServiceCollection services)
+        {
+            services.AddDbContext<ETicaretApiDbContext>(options => options.UseNpgsql());
+        }
+    }
+}
