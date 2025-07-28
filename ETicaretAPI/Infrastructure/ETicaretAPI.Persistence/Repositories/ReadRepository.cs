bedbe78f1e9c3d78494db5d0e42bde26c5b49f78
Cevat <khrmncvt@gmail.com>
2025-07-29 00:33:42 +0300

    Generic Repository Design Pattern oluşturuldu

---
+++ ETicaretAPI/Infrastructure/ETicaretAPI.Persistence/Repositories/ReadRepository.cs
@@ -0,0 +1,44 @@
+﻿using ETicaretApi.Persistence.Contexts;
+using ETicaretAPI.Application.Repositories;
+using ETicaretAPI.Domain.Entities.Common;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Persistence.Repositories
+{
+    public class ReadRepository<T> : IReadRepository<T> where T : BaseEntity
+    {
+        private readonly ETicaretApiDbContext _context;
+        public ReadRepository(ETicaretApiDbContext context)
+        {
+            _context = context;
+        }
+
+        public DbSet<T> Table => _context.Set<T>();
+
+        public IQueryable<T> GetAll()
+        {
+            return Table;
+        }
+
+        public IQueryable<T> GetWhere(Expression<Func<T, bool>> method)
+        {
+            return Table.Where(method);
+        }
+
+        public async Task<T> GetSingleAsync(Expression<Func<T, bool>> method)
+        {
+            return await Table.FirstOrDefaultAsync(method);
+        }
+
+        public async Task<T> GetByIdAsync(string id)
+        {
+            return await Table.FirstOrDefaultAsync(x => x.Id == Guid.Parse(id));
+        }
+    }
+}
